d48ffc2a8b836b4bc58d5375d25360b19a2d3b64
Jonathan Owens <jonathan.owens@coxautoinc.com>
2018-12-08 01:16:13 +0000

    changed app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            React running inside a docker container.
+            Project was modified on the Feature branch.
           </p>
           <a
             className="App-link"
